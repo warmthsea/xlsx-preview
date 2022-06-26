dfc4636f9805b498b697626db364a2ae18087f6e
Wanghua3819 <2586244885@qq.com>
2022-06-26 21:16:03 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,23 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), AutoImport({
+  plugins: [vue(),
+  AutoImport({
+    // 自动导入vue相关的Api
+    imports: ["vue"],   // 也支持vue-router、axios等
+    // 声明文件的存放位置
+    dts: 'auto-imports.d.ts',
+  }),
+  AutoImport({
     resolvers: [ElementPlusResolver()],
   }),
   Components({
     resolvers: [ElementPlusResolver()],
-  }),],
+  })
+  ],
   server: {
     host: "0.0.0.0", // 默认为localhost
     port: 4004, // 端口号
