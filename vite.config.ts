f839141913d9b81b63ae54613d6c4baf0e585b88
Wanghua3819 <2586244885@qq.com>
2022-06-26 21:02:34 +0800

    chrome：config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), AutoImport({
     resolvers: [ElementPlusResolver()],
   }),
